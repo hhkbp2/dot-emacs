d24a9d4e21c892d390d53f8d2f6f9aa6b3a1b19c
Dylan.Wen <hhkbp2@gmail.com>
2013-03-06 10:19:56 +0800

    skip whitespace chars customzation on office machine

--- site-lisp/whitespace-settings.el
+++ site-lisp/whitespace-settings.el
@@ -4,7 +4,7 @@
 ;; Copyright (C) 2012 Dylan.Wen
 
 ;; Author: Dylan.Wen <hhkbp2@gmail.com>
-;; Time-stamp: <2013-01-01 22:18>
+;; Time-stamp: <2013-03-06 10:14>
 
 ;; This file is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
@@ -31,14 +31,16 @@
 (defun whitespace-settings ()
   "Settings for `whitespace'."
 
-  ;; refer to http://ergoemacs.org/emacs/whitespace-mode.html
-  (setq whitespace-display-mappings
-        ;; all numbers are Unicode codepoint in decimal. e.g. (insert-char 182 1)
-        '(
-          (space-mark 32 [183] [46]) ; 32 SPACE 「 」, 183 MIDDLE DOT 「·」, 46 FULL STOP 「.」
-          (newline-mark 10 [8629 10]) ; 10 LINE FEED, 8629 DOWNWARDS ARROW WITH CORNER LEFTWARDS 「↵」
-          (tab-mark 9 [8677 9] [92 9]) ; 9 TAB, 8677 RIGHTWARDS ARROW TO BAR 「⇥」
-          ))
+  (unless (dw-on-office-machine)
+    ;; refer to http://ergoemacs.org/emacs/whitespace-mode.html
+    (setq whitespace-display-mappings
+          ;; all numbers are Unicode codepoint in decimal. e.g. (insert-char 182 1)
+          '(
+            (space-mark 32 [183] [46]) ; 32 SPACE 「 」, 183 MIDDLE DOT 「·」, 46 FULL STOP 「.」
+            (newline-mark 10 [8629 10]) ; 10 LINE FEED, 8629 DOWNWARDS ARROW WITH CORNER LEFTWARDS 「↵」
+            (tab-mark 9 [8677 9] [92 9]) ; 9 TAB, 8677 RIGHTWARDS ARROW TO BAR 「⇥」
+            ))
+    )
   )
 
 (eval-after-load "whitespace"
