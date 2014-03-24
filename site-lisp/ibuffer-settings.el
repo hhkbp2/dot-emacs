2729d59906b7a7954accb81e6a66e8fb3c8f6a97
Dylan.Wen <hhkbp2@gmail.com>
2014-03-24 15:48:58 +0800

    add go tag in ibuffer

--- site-lisp/ibuffer-settings.el
+++ site-lisp/ibuffer-settings.el
@@ -4,7 +4,7 @@
 ;; Copyright (C) 2011 Dylan.Wen
 
 ;; Author: Dylan.Wen <hhkbp2@gmail.com>
-;; Time-stamp: <2013-06-24 12:13>
+;; Time-stamp: <2014-03-24 15:48>
 
 ;; This file is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
@@ -87,6 +87,7 @@
                                (mode . scheme-interaction-mode)))
                  ("Python" (mode . python-mode))
                  ("Erlang" (mode . erlang-mode))
+                 ("Go" (mode . go-mode))
                  ("TeX" (or (mode . tex-mode)
                             (mode . latex-mode)))
                  ("Plan" (or (mode . calendar-mode)
