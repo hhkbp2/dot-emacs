028f1667bb618b8115cec060be677bbca97f53f1
Dylan.Wen <hhkbp2@gmail.com>
2014-08-31 23:56:21 +0800

    add font pair spec and rescale settings

--- site-lisp/font-settings.el
+++ site-lisp/font-settings.el
@@ -4,7 +4,7 @@
 ;; Copyright (C) 2013 Dylan.Wen
 
 ;; Author: Dylan.Wen <hhkbp2@gmail.com>
-;; Time-stamp: <2014-03-29 16:50>
+;; Time-stamp: <2014-08-31 23:55>
 
 ;; This file is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
@@ -24,27 +24,41 @@
 ;;; Code:
 
 
-(require 'cl)
-
-
 (defconst dw-favor-en-font-list
-  '("Envy Code R"
-    "Fantasque Sans Mono"
+  '("M+ 1mn"
+    "Envy Code R"
     "Monaco"
     "Menlo"
     "Source Code Pro"
     "Ubuntu Mono"
-    "WenQuanYi Micro Hei")
+    "Fantasque Sans Mono"
+    )
   "Personal favor font list for language en in descendent order.")
-
-(defconst dw-favor-font-size 15
-  "Personal favor font size for all kinds of font.")
 
 (defconst dw-favor-zh-font-list
   '("Hiragino Sans GB"
+    "WenQuanYi Micro Hei Mono"
     "微软雅黑Monaco"
-    "WenQuanYi Micro Hei")
+    )
   "Personal favor font list for language zh in descendent order.")
+
+(setq dw-font-pairs
+      '((("M+ 1mn" . "Hiragino Sans GB") .
+         ;; font-spec parameters, rescale ratio
+         (((:size 15) . 1.0) . ((:size 15) . 1.1)))
+        (("Monaco" . "Hiragino Sans GB") .
+         (((:size 14) . 1.0) . ((:size 14) . 1.2)))
+        (("Envy Code R" . "Hiragino Sans GB") .
+         (((:size 15) . 1.0) . ((:size 15) . 1.1)))
+        (("Source Code Pro" . "Hiragino Sans GB") .
+         (((:size 15) . 1.0) . ((:size 15) . 1.2)))
+        (("Ubuntu Mono" . "Hiragino Sans GB") .
+         (((:size 16) . 1.0) . ((:size 16) . 1.0)))
+        (("Fantasque Sans Mono" . "Hiragino Sans GB") .
+         (((:size 16) . 1.0) . ((:size 16) . 1.0)))
+        (("M+ 1mn" . "WenQuanYi Micro Hei Mono") .
+         (((:size 15) . 1.0) . ((:size 15) . 1.1)))
+        ))
 
 
 (defun dw-font-exsits-p (font-name-pattern)
@@ -55,57 +69,71 @@
   "Return first available font in `font-list'."
   (find-if #'dw-font-exsits-p font-list))
 
+(defun dw-font-pair-exists-p (pair)
+  (assoc pair dw-font-pairs))
 
-(defconst dw-default-font-name
-  (if (display-graphic-p)
-      (dw-first-available-font dw-favor-en-font-list)
-    (first dw-favor-en-font-list))
-  "Default frame font name.")
-
-(defconst dw-default-font
-  (format "%s-%s" dw-default-font-name dw-favor-font-size)
-  "Default frame font desc.")
-
-
-(defun dw-set-font (en-font-list
-                    en-font-size
-                    zh-font-list
-                    &optional zh-font-size)
-  "Set font for language en and zh.
-`en-font-list' and `zh-font-list' should be a list of font name string.
-`en-font-size' should be of integer. If `zh-font-size' if omitetd,
-its value comes from `en-font-size'."
-
-  (or zh-font-size (setq zh-font-size en-font-size))
+(defun dw-available-font-pair (en-font-list zh-font-list)
   (let* ((en-font-name (dw-first-available-font en-font-list))
-         (zh-font-name (dw-first-available-font zh-font-list)))
+         (zh-font-name (dw-first-available-font zh-font-list))
+         (font-pair (cons en-font-name zh-font-name)))
     (unless (and en-font-name zh-font-name)
       (error "no available font in font list en: %s and zh: %s"
              en-font-list zh-font-list))
-    ;; set en font
-    (set-face-attribute 'default nil
-                        :font (format "%s %s" en-font-name en-font-size))
-    ;; set zh font
-    (dolist (charset '(kana han symbol cjk-misc bopomofo))
-      (set-fontset-font (frame-parameter nil 'font) charset
-                        (font-spec :family zh-font-name :size zh-font-size)))))
+    (unless (dw-font-pair-exists-p font-pair)
+      (error "no available font pair for font en: %s and zh: %s"
+             en-font-name zh-font-name))
+    font-pair))
 
-(defun dw-set-frame-font ()
-  "Set the frame default font."
-  (set-frame-font dw-default-font)
-  (add-to-list 'default-frame-alist `(font . ,dw-default-font)))
+(defun dw-font-spec-for-pair (pair)
+  (let ((pair-parameters (cdr (assoc pair dw-font-pairs))))
+    (cl-flet ((get-font-spec (name paras)
+                             (apply #'font-spec :family name paras)))
+      (cl-values (get-font-spec (car pair) (car (car pair-parameters)))
+                 (get-font-spec (cdr pair) (car (cdr pair-parameters)))))))
+
+(defun dw-font-rescale-alist-for-pair (pair)
+  (let ((pair-parameters (cdr (assoc pair dw-font-pairs))))
+    (cl-flet ((get-regexp (font-name)
+                          (replace-regexp-in-string "\\s-" "." font-name)))
+      (list  (cons (get-regexp (car pair)) (cdr (car pair-parameters)))
+             (cons (get-regexp (cdr pair)) (cdr (cdr pair-parameters)))))))
+
+
+(defun dw-set-font (en-font-list zh-font-list)
+  "Set font for language en and zh.
+`en-font-list' and `zh-font-list' should be a list of font name string.
+It will try to find out the existing font in each list to perform a proper
+settings according to `dw-font-pairs'."
+
+  (let* ((font-pair (dw-available-font-pair en-font-list zh-font-list)))
+    ;; simulate to multiple return values
+    (cl-multiple-value-bind
+        (en-font-spec zh-font-spec) (dw-font-spec-for-pair font-pair)
+      ;; (set-face-attribute 'default t
+      ;;                     :font en-font-spec  :weight 'normal :width 'normal
+      ;; set en font
+      (set-frame-font en-font-spec)
+      ;; set zh font
+      (dolist (charset '(kana han symbol cjk-misc bopomofo))
+        (set-fontset-font (frame-parameter nil 'font) charset
+                          zh-font-spec)))))
+
+
+(defadvice frame-notice-user-settings (before dw-rescale-alist)
+  (let ((font-pair (dw-available-font-pair
+                    dw-favor-en-font-list dw-favor-zh-font-list)))
+    (dolist (elem (dw-font-rescale-alist-for-pair font-pair))
+      (add-to-list 'face-font-rescale-alist
+                   elem))))
+(ad-activate 'frame-notice-user-settings)
 
 
 (defun font-settings ()
   "Settings for font."
 
   (when (display-graphic-p)
-    ;; font setting
-    (dw-set-frame-font)
-    (dw-set-font dw-favor-en-font-list
-                 dw-favor-font-size
-                 dw-favor-zh-font-list
-                 (1- dw-favor-font-size))))
+    (setq-default line-spacing nil)
+    (dw-set-font dw-favor-en-font-list dw-favor-zh-font-list)))
 
 
 (provide 'font-settings)
