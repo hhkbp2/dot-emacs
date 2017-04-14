af4823d95d80b0cba7091fb14e19408a421bf15b
Dylan.Wen <hhkbp2@gmail.com>
2017-04-14 23:55:28 +0800

    add GOPATH detection for goimport tidb codes

--- site-lisp/go-mode-settings.el
+++ site-lisp/go-mode-settings.el
@@ -4,7 +4,7 @@
 ;; Copyright (C) 2014 Dylan.Wen
 
 ;; Author: Dylan.Wen <hhkbp2@gmail.com>
-;; Time-stamp: <2017-03-30 15:15>
+;; Time-stamp: <2017-04-15 23:59>
 
 ;; This file is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
@@ -28,6 +28,26 @@
 (require 'dw-functionals)
 (require 'go-mode)
 
+(defun dw-go-tidb-gopath ()
+  "Detect GOPATH for the tidb project."
+  (seq-some #'dw-go--tidb-gopath ["_vendor" "vendor"]))
+
+(defun dw-go--tidb-gopath (filename)
+  "Detect GOPATH using the hint FILENAME."
+  (let* ((d (locate-dominating-file buffer-file-name "tidb"))
+         (tidb-root (concat d (file-name-as-directory "tidb")))
+         (vendor (concat tidb-root (file-name-as-directory filename))))
+    (if (and d
+             (file-exists-p vendor))
+        (cons vendor (go-plain-gopath)))))
+
+(defadvice gofmt (before gofmt-prepare-gopath activate)
+  "Detect GOPATH and set it before running `gofmt'."
+  (go-set-project))
+
+(defadvice gofmt (after gofmt-reset-gopath activate)
+  "Reset GOPATH to the value it had before running `gofmt'."
+  (go-reset-gopath))
 
 (defun go-mode-settings ()
   "Settings for `go-mode'."
@@ -36,6 +56,9 @@
   ;; non `go-mode' buffer would be intact
   (setq gofmt-command "goimports")
   (add-hook 'before-save-hook 'gofmt-before-save)
+
+  ;; add the gopath guessing function for tidb
+  (add-to-list 'go-guess-gopath-functions #'dw-go-tidb-gopath)
 
   ;; key bindings
   (dw-hungry-delete-on-mode-map go-mode-map)
