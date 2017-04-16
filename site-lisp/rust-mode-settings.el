19da13d934302c5fd0df7c669f29749e4f6fe5a9
Dylan.Wen <hhkbp2@gmail.com>
2017-04-16 21:05:09 +0800

    remove header LICENSE

--- site-lisp/rust-mode-settings.el
+++ site-lisp/rust-mode-settings.el
@@ -1,24 +1,6 @@
 ;;; rust-mode-settings.el --- Settings for `rust-mode'
 ;; -*- Emacs-Lisp -*-
 
-;; Copyright (C) 2016 Dylan.Wen
-
-;; Author: Dylan.Wen <hhkbp2@gmail.com>
-;; Time-stamp: <2017-02-04 10:15>
-
-;; This file is free software: you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation, either version 3 of the License, or
-;; (at your option) any later version.
-
-;; This file is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;; GNU General Public License for more details.
-
-;; You should have received a copy of the GNU General Public License
-;; along with this file.  If not, see <http://www.gnu.org/licenses/>.
-
 ;;; Commentary:
 
 ;;; Code:
