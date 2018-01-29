392b8fb9a08c96a86e618be4dceee8b7d9ed86c3
Dylan.Wen <hhkbp2@gmail.com>
2018-01-29 15:09:50 +0800

    restore fonts sizes

--- site-lisp/base/font-settings.el
+++ site-lisp/base/font-settings.el
@@ -34,9 +34,9 @@
         (("Monaco" . "Hiragino Sans GB") .
          (((:size 14) . 1.0) . ((:size 14) . 1.2)))
         (("Monaco" . "思源宋体") .
-         (((:size 13) . 1.0) . ((:size 13) . 1.0)))
+         (((:size 14) . 1.0) . ((:size 14) . 1.0)))
         (("Monaco" . "思源黑体") .
-         (((:size 13) . 1.0) . ((:size 15) . 1.0)))
+         (((:size 14) . 1.0) . ((:size 16) . 1.0)))
         (("M+ 1mn" . "WenQuanYi Micro Hei Mono") .
          (((:size 15) . 1.0) . ((:size 15) . 1.1)))
         ))
