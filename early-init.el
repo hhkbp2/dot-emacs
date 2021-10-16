8a2d7cb5832da8b8ecf2a00dd180522ac4b9d41f
Dylan.Wen <hhkbp2@gmail.com>
2021-10-17 02:22:00 +0800

    add early-init.el for initialization

---
+++ early-init.el
@@ -0,0 +1,22 @@
+;; early-init.el -*- lexical-binding: t; -*-
+
+;; Emacs 27 introduces early-init.el, which is run before init.el,
+;; before package and UI initialization happens.
+
+;; Defer garbage collection further back in the startup process
+(setq gc-cons-threshold most-positive-fixnum)
+
+;; Package initialize occurs automatically, before `user-init-file' is
+;; loaded, but after `early-init-file'. We handle package
+;; initialization, so we must prevent Emacs from doing it early!
+(setq package-enable-at-startup nil)
+
+;; Prevent the glimpse of un-styled Emacs by disable these UI elements early.
+(push '(menu-bar-lines . 0) default-frame-alist)
+(push '(tool-bar-lines . 0) default-frame-alist)
+(push '(vertical-scroll-bars) default-frame-alist)
+
+;; Resizing the Emacs frame can be a terribly expensive part of changing the
+;; font. By inhibiting this, we easily halve startup times with fonts that are
+;; larger than the system default.
+(setq frame-inhibit-implied-resize t)
