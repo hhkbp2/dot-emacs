1965af911da020c061cc4671735483803fe762e1
Dylan.Wen <hhkbp2@gmail.com>
2023-06-17 15:53:08 +0800

    add geiser settings

--- site-lisp/lang/scheme/scheme-mode-settings.el
+++ site-lisp/lang/scheme/scheme-mode-settings.el
@@ -9,8 +9,15 @@
   :ensure t
   :config
   (progn
-    (setq geiser-active-implementations '(guile)))
-  )
+    (setq geiser-active-implementations '(guile)
+          geiser-repl-history-filename "~/.emacs.d/geiser-history"
+          geiser-repl-query-on-kill-p nil
+          geiser-repl-query-on-exit-p nil)
+    (with-eval-after-load 'geiser-guile
+      (add-to-list 'geiser-guile-load-path
+                   "~/dev/pro/lang/lisp/scheme/sicp/exercise/sicp"))
+    ))
+
 
 (use-package scheme
   :defer t
